858ad7f32e1e4aeb92e95b756dfd6a34cf5bdd65
Meg <mmwoodfo@asu.edu>
2023-11-04 12:51:46 -0700

    Setting up unit tests

---
+++ FinanceApp.Tests/TestData.cs
@@ -0,0 +1,35 @@
+﻿using FinanceApp.Data.Models;
+
+namespace FinanceApp.Tests;
+public static class TestData
+{
+    public static ApplicationUser CreateTestApplicationUser()
+    {
+        return new ApplicationUser()
+        {
+            Name = "Test User",
+            Email = "Test@FinanceApp.com",
+            AzureObjectId = "123abc"
+        };
+    }
+
+    public static Budget CreateTestBudget()
+    {
+        return new Budget()
+        {
+            Name = "Test Budget"
+        };
+    }
+
+    public static BudgetUser CreateTestBudgetUser(Budget? budget = null, ApplicationUser? appUser = null)
+    {
+        budget = budget ?? CreateTestBudget();
+        appUser = appUser ?? CreateTestApplicationUser();
+
+        return new BudgetUser()
+        {
+            Budget = budget,
+            ApplicationUser = appUser
+        };
+    }
+}
